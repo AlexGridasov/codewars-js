4653d848202bdc66ca9ff6205e3774d29c236f08
Alex Gridasov <palleev@gmail.com>
2016-03-05 19:30:23 +0200

    Added dependencies. Working test with calculator

---
+++ src/js/calculator.js
@@ -0,0 +1,11 @@
+var calculator = {
+    sum: function (x, y) {
+        return x + y;
+    },
+    subtract: function (x, y) {
+        return x - y;
+    },
+    divide: function (x, y) {
+        return (y === 0) ? 0 : x / y;
+    }
+};
