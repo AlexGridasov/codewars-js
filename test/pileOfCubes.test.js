7964ea13fdb31a41e34fef73f85a6aa5f2af91e3
Alexander Gridasov <palleev@gmail.com>
2016-03-08 12:13:14 +0200

    Added test for Pile of Cubes and function stub

---
+++ test/pileOfCubes.test.js
@@ -0,0 +1,26 @@
+/**
+ * Created by alex on 8/3/16.
+ */
+describe('Pile of Cubes', function () {
+
+    describe('findNb', function () {
+        it('Number of cubes should be 2022', function () {
+            expect(PileOfCubes.findNb(4183059834009)).toBe(2022);
+        });
+        it('Number of cubes should be -1', function () {
+            expect(PileOfCubes.findNb(24723578342962)).toBe(-1);
+        });
+        it('Number of cubes should be 4824', function () {
+            expect(PileOfCubes.findNb(135440716410000)).toBe(4824);
+        });
+        it('Number of cubes should be 3568', function () {
+            expect(PileOfCubes.findNb(40539911473216)).toBe(3568);
+        });
+    });
+
+
+    /*Test.assertEquals(findNb(4183059834009), 2022)
+    Test.assertEquals(findNb(24723578342962), -1)
+    Test.assertEquals(findNb(135440716410000), 4824)
+    Test.assertEquals(findNb(40539911473216), 3568)*/
+});
